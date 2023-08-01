217ae304794f02636b8e95743c5ffcc97a0385dd
Lucas Marshall <lucas@supergrain.com>
2023-08-01 12:29:26 -0700

    scaffolding

---
+++ index.ts
@@ -0,0 +1,18 @@
+// Import necessary modules
+import express, {json} from 'express';
+
+const app = express();
+
+// Define middleware
+app.use(json());
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
